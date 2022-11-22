4e7bbff263171903c20f917ebe4a821d2c74f296
Edmund Brotz <erb1851@rit.edu>
2022-11-22 14:23:06 -0500

    Structure and Packet FIlter
    add basic file structure and packet filter completed

---
+++ filter_packets.py
@@ -0,0 +1,15 @@
+def filter(inputFile, outputFile):
+	infile = open(inputFile, 'r')
+	outfile = open(outputFile, "a")
+	fileline = infile.readline()
+	outfile.write(fileline)
+	while fileline:
+		if "Echo" in fileline:
+			outfile.write(fileline)
+			while fileline.startswith("No.") == False:
+				fileline = infile.readline()
+				outfile.write(fileline)
+		fileline = infile.readline()
+	infile.close()
+	outfile.close()
+filter("Node1.txt", "Filered1.txt")
