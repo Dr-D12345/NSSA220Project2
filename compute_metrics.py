c2b1245e822cb46e78e3b3babcb7e21a2e96860b
Alex Habrat <beastboyfan53@gmail.com>
2022-12-01 23:17:13 -0500

    Penny Trading

--- compute_metrics.py
+++ compute_metrics.py
@@ -9,32 +9,7 @@ def compute(host, L) :
 	replies_received=0
 	i=0
 	Rtt=0.0
-	"""	
-	for item in L:
-		type = item[6]["type"]
-		if (item[2] == host):
-			if (type == 'request'):
-				request_sent += 1
-				request_bytes_sent += int(item[5])
-				request_data_sent += (int(item[5])-42)
-				request_time = float(item[1])
-				i+=1
-			elif (type == 'reply'):
-				replies_sent += 1
-				Rtt += (float(item[1])-request_time)*1000
-		elif (item[3] == host):
-			if (type == 'request'):
-				request_received +=1
-				request_bytes_received += int(item[5])
-				request_data_received +=(int(item[5])-42)
-				request_time = float(item[1])
-				i+=1
-			elif (type == 'reply'):
-				replies_received += 1
-				Rtt += (float(item[1])-request_time)*1000
-	"""
-	i=0
-	request_time =0
+	reply_delay=0.0
 	for x in range(0, len(L)):
 		item = L[x]
 		type = item[6]["type"].lower()
@@ -43,17 +18,22 @@ def compute(host, L) :
 		if(type == 'request'):
 			if(requestHost == host):
 				request_sent += 1
+				request_bytes_sent += int(item[5])
+				request_data_sent += (int(item[5])-42)
 				request_time = float(item[1])
 			else:
 				request_received +=1
-			request_bytes_sent += int(item[5])
-			request_data_sent += (int(item[5])-42)
+				request_bytes_received += int(item[5])
+				request_data_received +=(int(item[5])-42)
+				receive_time = float(item[1])
 		elif(type == 'reply'):
 			if(requestHost == host):
 				replies_sent += 1
+				reply_delay += (float(item[1]) - receive_time)*10000
 			else:
 				replies_received +=1
 				Rtt += (float(item[1])-request_time) *1000
 				i+=1
 	Avg_Rtt=Rtt/float(i)
-	print(Avg_Rtt)
+	Avg_reply_delay = reply_delay/float(i)
+	print(Avg_reply_delay)
