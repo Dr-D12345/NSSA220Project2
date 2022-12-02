ad229f843f07e79fe2087c6e2e5e5a3745af4532
Alex Habrat <beastboyfan53@gmail.com>
2022-12-01 20:18:53 -0500

    beginning metrics

--- compute_metrics.py
+++ compute_metrics.py
@@ -1,3 +1,29 @@
-def compute() :
-	print('called compute function in compute_metrics.py')
-
+def compute(host, L) :
+	request_sent = 0
+	request_bytes_sent=0
+	request_data_sent=0
+	request_received = 0
+	request_bytes_received=0
+	request_data_received=0
+	replies_sent =0
+	replies_received=0
+	i=0
+	RTT=0
+	for item in L:
+		if (item[2] == host):
+			info = item[6].split()
+			if (info[2] == 'request'):
+				request_sent += 1
+				request_bytes_sent += int(item[5])
+				request_data_sent += (int(item[5])-42)
+			elif (info[2] == 'reply'):
+				replies_sent += 1
+		elif (item[3] == host):
+			info = item[6].split()
+			if (info[2] == 'request'):
+				request_received +=1
+				request_bytes_received += int(item[5])
+				request_data_received +=(int(item[5])-42)
+			elif (info[2] == 'reply'):
+				replies_received += 1
+    
